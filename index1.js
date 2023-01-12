0ced4830522ab69e99948b0ed10875baca7d9783
Seanghor <hai.seanghor009@gmail.com>
2023-01-12 14:58:55 +0700

    qqw

--- index1.js
+++ index1.js
@@ -5,3 +5,8 @@ console.log('====================================');
 
 console.log("add new line");
 console.log("add new line 2");
+function hello(name) {
+    return `Hello ${name}`
+}
+const a = hello("seanghor")
+console.log(a);
