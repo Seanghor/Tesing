b775aa298d706adfb31df1dcd0fdf539dcbe3352
Seanghor <hai.seanghor009@gmail.com>
2023-01-12 15:34:47 +0700

    ertert

--- index1.js
+++ index1.js
@@ -2,8 +2,3 @@ console.log('====================================');
 console.log("file index 1");
 console.log('====================================');
 
-
-function say(greeting) {
-    return `${greeting} Seanghor`
-}
-console.log(say("Good morning"))
